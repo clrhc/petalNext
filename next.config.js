c86de95f335e064a79cbe1ae818367176d534969
Celerity <103552735+clrhc@users.noreply.github.com>
2025-09-24 19:09:48 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/01997e69-306e-b9db-dbf4-a3624ed3ab01',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
