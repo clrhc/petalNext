57253df698213aa0d91098d9f6682592b22a687c
Celerity <103552735+clrhc@users.noreply.github.com>
2025-09-26 22:37:17 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ const nextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
