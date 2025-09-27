adff8f5f6c9305ed668e5b7ed5d44deb9dd1ac5c
Celerity <103552735+clrhc@users.noreply.github.com>
2025-09-27 16:22:24 -0600

    Update wagmiConfig.tsx

--- app/components/wagmiConfig.tsx
+++ app/components/wagmiConfig.tsx
@@ -7,7 +7,7 @@ export const config = createConfig({
   chains: [base],
   connectors: [injected()],
   transports: {
-    [base.id]: http('https://base-mainnet.infura.io/v3/cc877a2fcbd848a89360422e704227d3'), // public RPC or your Alchemy/Infura key
+    [base.id]: http('https://base-mainnet.public.blastapi.io'), // public RPC or your Alchemy/Infura key
   },
   ssr: true,
 });
