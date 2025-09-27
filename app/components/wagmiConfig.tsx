d37ad6d8ca805927102555a89074a1304c209f0d
Celerity <103552735+clrhc@users.noreply.github.com>
2025-09-26 21:08:26 -0600

    a

--- app/components/wagmiConfig.tsx
+++ app/components/wagmiConfig.tsx
@@ -5,7 +5,7 @@ import { base } from 'wagmi/chains';
 export const config = createConfig({
   chains: [base],
   transports: {
-    [base.id]: http('https://base-mainnet.infura.io/v3/cc877a2fcbd848a89360422e704227d3'), // public RPC or your Alchemy/Infura key
+    [base.id]: http('https://base-mainnet.public.blastapi.io'), // public RPC or your Alchemy/Infura key
   },
   ssr: true, // ✅ if you're using SSR in Next.js
 });
