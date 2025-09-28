13421647cc9eb8f1d6d01337ab454157ee5973d0
Celerity <103552735+clrhc@users.noreply.github.com>
2025-09-27 18:16:43 -0600

    Update index.tsx

--- app/context/index.tsx
+++ app/context/index.tsx
@@ -1,6 +1,5 @@
 'use client'; 
 import { MiniKitProvider } from '@coinbase/onchainkit/minikit';
-import { OnchainKitProvider } from '@coinbase/onchainkit';
 import { wagmiAdapter, projectId } from '../config';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createAppKit } from '@reown/appkit/react';
