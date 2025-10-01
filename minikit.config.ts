741ad94565457ed34e041a9a2f54405e03990d54
Celerity <103552735+clrhc@users.noreply.github.com>
2025-10-01 02:49:22 -0600

    Update minikit.config.ts

--- minikit.config.ts
+++ minikit.config.ts
@@ -12,7 +12,6 @@ export const minikitConfig = {
     imageUrl: "https://i.imgur.com/6fsw46l.png",
     buttonTitle: "Register Now for Free Rewards!",
     splashImageUrl: "https://i.imgur.com/pYoZQlK.png",
-    webhookUrl: `https://petal.wtf/api/webhook`,
     splashBackgroundColor: "#1e534c",
     subtitle: "Register Now for Free Rewards!",
     description: "V2 DeFi and Meme Markets",
