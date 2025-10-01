d375cbe1b594f6d855d80deddbc13143cd4cf9e3
Celerity <103552735+clrhc@users.noreply.github.com>
2025-09-30 20:23:21 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,10 +22,28 @@ export async function generateMetadata(): Promise<Metadata> {
                     iconUrl: "https://i.imgur.com/cOl0Utj.png",
                     splashImageUrl: 'https://i.imgur.com/pYoZQlK.png',
                     splashBackgroundColor: '#1e534c',
-                    subtitle: "V2 DeFi and Meme Markets",
-                    description: "Register Now for Free Rewards!",
+                    subtitle: "Register Now for Free Rewards!",
+                    description: "V2 DeFi and Meme Markets",
+                    homeUrl: "https://petal.wtf",
+                    buttonTitle: "Register Now for Free Rewards!",
+                    screenshotUrls: ["https://i.imgur.com/7DgS91f.png"],
+                    primaryCategory: "finance",
+                    tags: ["miniapp","baseapp","defi"],
+                    heroImageUrl: "https://i.imgur.com/cOl0Utj.png",
+                    tagline: "Register Now for Free Rewards!",
+                    ogTitle: "Petal Finance",
+                    ogDescription: "Register Now for Free Rewards!",
+                    ogImageUrl: "https://i.imgur.com/6fsw46l.png"
                 },
             },
+        }),
+         'baseBuilder': JSON.stringify({
+          allowedAddresses: ["0x0870dF064d160f40c8F6c966dCa25db9326b23F4"],
+        }),
+         'accountAssociation': JSON.stringify({
+          header: "eyJmaWQiOjI5MzE5NCwidHlwZSI6ImF1dGgiLCJrZXkiOiIweDY4Yjk3RDg5ODk0OUE2NzgxZDc3OUU0Y0U1NEVDRjQyQTJhM2NCNWQifQ",
+          payload: "eyJkb21haW4iOiIifQ",
+          signature: "DoUssDrbzR1HEYW4Y8EIlBF7jeKyZEcaDowwlr9pKF8qxMul5t1jE6F6AkWt+H420xYKOucS3qSBYfs+RqNBSRw="
         }),
         },
     };
